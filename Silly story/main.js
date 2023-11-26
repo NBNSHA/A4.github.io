c78944c9b614a897e6ec6abc9b644e0ab0584701
NBNSHA <144839907+NBNSHA@users.noreply.github.com>
2023-11-25 22:27:24 -0500

    updating the main.js

--- Silly story/main.js
+++ Silly story/main.js
@@ -1,6 +1,6 @@
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+var customName = document.getElementById('customname');
+var randomize = document.querySelector('.randomize');
+var story = document.querySelector('.story');
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
